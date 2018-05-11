9f4f911995935823cd1a48dcc644f71c7fe60d35
Christiaan Frans Rademan <christiaan.rademan@gmail.com>
2018-05-11 15:32:58 +0200

    Should not reference infinitystone

--- photonic/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from infinitystone.models.domains import luxon_domain
-from infinitystone.models.endpoints import luxon_endpoint
-from infinitystone.models.roles import luxon_role
-from infinitystone.models.tenants import luxon_tenant
-from infinitystone.models.user_roles import luxon_user_role
-from infinitystone.models.users import luxon_user
